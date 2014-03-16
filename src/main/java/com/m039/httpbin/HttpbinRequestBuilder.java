49e799c978be6b2c3539a80274777a639c495f1a
Dmitry Mozgin <m0391n@gmail.com>
2014-03-17 02:19:37 +0400

    Add code and tests

--- src/main/java/com/m039/httpbin/HttpbinRequestBuilder.java
+++ src/main/java/com/m039/httpbin/HttpbinRequestBuilder.java
@@ -1,25 +1,313 @@
-/** HttpbinRequestBuilder.java --- 
- *
- * Copyright (C) 2014 Dmitry Mozgin
- *
- * Author: Dmitry Mozgin <m0391n@gmail.com>
- *
- * 
+/** HttpbinRequestBuilder.java ---
  */
 
 package com.m039.httpbin;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Map;
+
 /**
- * 
+ * Testing an HTTP Library can become difficult sometimes. <a
+ * href="http://postbin.org">Postbin</a> is fantastic for testing POST
+ * requests, but not much else. This exists to cover all kinds of HTTP
+ * scenarios. Additional endpoints are being considered
+ * (e.g. /deflate).
+ *
+ * All endpoint responses are JSON-encoded.
+ *
+ * @see <a href="http://httpbin.org">http://httpbin.org</a>
  *
  * Created: 03/16/14
  *
  * @author Dmitry Mozgin
- * @version 
- * @since 
  */
 public class HttpbinRequestBuilder {
 
-    static public String A = "a";
+    static private String BASE_URL = "http://httpbin.org";
+
+    /**
+     * / This page.
+     */
+    static public String baseUrl() {
+        return BASE_URL;
+    }
+
+    /**
+     * /ip Returns Origin IP.
+     */
+    static public String ip() {
+        return BASE_URL + "/ip";
+    }
+
+    /**
+     * /user-agent Returns user-agent.
+     */
+    static public String userAgent() {
+        return BASE_URL + "/user-agent";
+    }
+
+    /**
+     * /headers Returns header dict.
+     */
+    static public String headers() {
+        return BASE_URL + "/headers";
+    }
+
+    /**
+     * /get Returns GET data.
+     */
+    static public String get() {
+        return BASE_URL + "/get";
+    }
+
+    /**
+     * /post Returns POST data.
+     */
+    static public String post() {
+        return BASE_URL + "/post";
+    }
+
+    /**
+     * /put Returns PUT data.
+     */
+    static public String put() {
+        return BASE_URL + "/put";
+    }
+
+    /**
+     * /patch Returns PATCH data.
+     */
+    static public String patch() {
+        return BASE_URL + "/patch";
+    }
+
+    /**
+     * /delete Returns DELETE data
+     */
+    static public String delete() {
+        return BASE_URL + "/delete";
+    }
+
+    /**
+     * /gzip Returns gzip-encoded data.
+     */
+    static public String gzip() {
+        return BASE_URL + "/gzip";
+    }
+
+    /**
+     * /html Renders an HTML Page.
+     */
+    static public String html() {
+        return BASE_URL + "/html";
+    }
+
+    /**
+     * /robots.txt Returns some robots.txt rules.
+     */
+    static public String robots() {
+        return BASE_URL + "/robots.txt";
+    }
+
+    /**
+     * /deny Denied by robots.txt file.
+     */
+    static public String deny() {
+        return BASE_URL + "/deny";
+    }
+
+    /**
+     * /cache 200 unless If-Modified-Since was sent, then 304.
+     */
+    static public String cache() {
+        return BASE_URL + "/cache";
+    }
+
+    /**
+     * /status/:code Returns given HTTP Status code.
+     *
+     * @param httpStatusCode
+     */
+    static public String status(String httpStatusCode) {
+        if (httpStatusCode == null) {
+            throw new IllegalArgumentException("httpStatusCode is null");
+        }
+
+        return BASE_URL + "/status/" + httpStatusCode;
+    }
+
+    /**
+     * /response-headers?key=val Returns given response headers.
+     *
+     * @param keyValuePairs
+     */
+    static public String responseHeaders(Map<String, String> keyValuePairs) {
+        return BASE_URL + "/response-headers" + toQueryString(keyValuePairs);
+    }
+
+    /**
+     * /redirect/:n 302 Redirects n times.
+     *
+     * @param times times to redirect
+     */
+    static public String redirect(int times) {
+        return BASE_URL + "/redirect/" + times;
+    }
+
+    /**
+     * /redirect-to?url=foo 302 Redirects to the foo URL.
+     *
+     * @param url url to redirect to
+     */
+    static public String redirectTo(String url) {
+        return BASE_URL + "/redirect-to?url=" + url;
+    }
+
+    /**
+     * /relative-redirect/:n 302 Relative redirects n times.
+     *
+     * @param times times to redierect
+     */
+    static public String relativeRedirect(int times) {
+        return BASE_URL + "/relative-redirect/" + times;
+    }
+
+    /**
+     * /cookies Returns cookie data.
+     */
+    static public String cookies() {
+        return BASE_URL + "/cookies";
+    }
+
+    /**
+     * /cookies/set?name=value Sets one or more simple cookies.
+     *
+     * @param keyValuePairs
+     */
+    static public String cookiesSet(Map<String, String> keyValuePairs) {
+        return BASE_URL + "/cookies/set" + toQueryString(keyValuePairs);
+    }
+
+    /**
+     * /cookies/delete?name Deletes one or more simple cookies.
+     *
+     * @param listOfNames
+     */
+    static public String cookiesDelete(String ... listOfNames) {
+        return BASE_URL + "/cookies/delete" + toQueryString(listOfNames);
+    }
+
+    /**
+     * /basic-auth/:user/:passwd HTTPBasic Auth.
+     *
+     * @param user
+     * @param password
+     */
+    static public String basicAuth(String user, String password) {
+        return BASE_URL + "/basic-auth/" + user + "/" + password;
+    }
+
+    /**
+     * /hidden-basic-auth/:user/:passwd 404'd BasicAuth.
+     *
+     * @param user
+     * @param password
+     */
+    static public String hiddenBasicAuth(String user, String password) {
+        return BASE_URL + "/hidden-basic-auth/" + user + "/" + password;
+    }
+
+    /**
+     * /digest-auth/:qop/:user/:passwd HTTP Digest Auth.
+     *
+     * @param qop
+     * @param user
+     * @param password
+     */
+    static public String digestAuth(String qop, String user, String password) {
+        return BASE_URL + "/digest-auth/" + qop + "/" + user + "/" + password;
+    }
+
+    /**
+     * /stream/:n Streams n–100 lines.
+     *
+     * @param lines
+     */
+    static public String stream(int lines) {
+        return BASE_URL + "/stream/" + lines;
+    }
+
+    /**
+     * /delay/:n Delays responding for n–10 seconds.
+     *
+     * @param delayInSeconds
+     */
+    static public String delay(int delayInSeconds) {
+        return BASE_URL + "/delay/" + delayInSeconds;
+    }
+
+    /**
+     * /drip?numbytes=400&duration=3&delay=1 Drip drip drip.
+     *
+     * @param numbytes
+     * @param duration
+     * @param delay
+     */
+    static public String drip(int numbytes, int duration, int delay) {
+        String pairs[] = new String[] {
+            "numbytes=" + numbytes,
+            "duration=" + duration,
+            "delay=" + delay
+        };
+
+        return BASE_URL + "/drip" + toQueryString(pairs);
+    }
+
+    static private String toQueryString(String listOfNames[]) {
+        if (listOfNames == null) {
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (String name : listOfNames) {
+            sb.append(name + "&");
+        }
+
+        return toQueryString(sb);
+    }
+
+    static private String toQueryString(Map<String, String> args) {
+        if (args == null) {
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            for (Map.Entry<String, String> pair : args.entrySet()) {
+                String key = pair.getKey();
+                String value = pair.getValue();
+
+                sb.append(String.format("%s=%s&", key, URLEncoder.encode(value, "UTF-8")));
+            }
+        } catch (UnsupportedEncodingException e) {
+            // ignore
+        }
+
+        return toQueryString(sb);
+    }
+
+    static private String toQueryString(StringBuilder sb) {
+        int length = sb.length();
+
+        if (length > 0) {
+            sb.delete(length - 1, length);
+            return "?" + sb.toString();
+        } else {
+            return sb.toString();
+        }
+    }
 
 } // HttpbinRequestBuilder
--- src/test/java/com/m039/httpbin/HttpbinRequestBuilderTest.java
+++ src/test/java/com/m039/httpbin/HttpbinRequestBuilderTest.java
@@ -1,32 +1,125 @@
-/** HttpbinRequestBuilderTest.java --- 
+/** HttpbinRequestBuilderTest.java ---
  *
  * Copyright (C) 2014 Dmitry Mozgin
  *
  * Author: Dmitry Mozgin <m0391n@gmail.com>
  *
- * 
+ *
  */
 
 package com.m039.httpbin;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import junit.framework.Assert;
 
 import org.junit.Test;
 
 /**
- * 
+ *
  *
  * Created: 03/16/14
  *
  * @author Dmitry Mozgin
- * @version 
- * @since 
+ * @version
+ * @since
  */
 public class HttpbinRequestBuilderTest {
-    
+
     @Test
-	public void testA() {
-        Assert.assertEquals(HttpbinRequestBuilder.A, "ad");
-	}
-    
+    public void testConstRequest() {
+        //
+        // All of this tests are silly
+        //
+
+        assertEquals(HttpbinRequestBuilder.baseUrl(), "http://httpbin.org");
+        assertEquals(HttpbinRequestBuilder.ip(), "http://httpbin.org/ip");
+        assertEquals(HttpbinRequestBuilder.userAgent(), "http://httpbin.org/user-agent");
+        assertEquals(HttpbinRequestBuilder.headers(), "http://httpbin.org/headers");
+        assertEquals(HttpbinRequestBuilder.get(), "http://httpbin.org/get");
+        assertEquals(HttpbinRequestBuilder.post(), "http://httpbin.org/post");
+        assertEquals(HttpbinRequestBuilder.put(), "http://httpbin.org/put");
+        assertEquals(HttpbinRequestBuilder.patch(), "http://httpbin.org/patch");
+        assertEquals(HttpbinRequestBuilder.delete(), "http://httpbin.org/delete");
+        assertEquals(HttpbinRequestBuilder.gzip(), "http://httpbin.org/gzip");
+        assertEquals(HttpbinRequestBuilder.html(), "http://httpbin.org/html");
+        assertEquals(HttpbinRequestBuilder.robots(), "http://httpbin.org/robots.txt");
+        assertEquals(HttpbinRequestBuilder.deny(), "http://httpbin.org/deny");
+        assertEquals(HttpbinRequestBuilder.cache(), "http://httpbin.org/cache");
+        assertEquals(HttpbinRequestBuilder.cookies(), "http://httpbin.org/cookies");
+    }
+
+    @Test
+    public void testVariableRequest() {
+        String baseUrl = "http://httpbin.org";
+        Map<String, String> map = new HashMap<String, String>();
+
+        // status
+        assertEquals(HttpbinRequestBuilder.status("300"), baseUrl + "/status/300");
+
+        // response-headers
+        map.clear();
+        assertEquals(HttpbinRequestBuilder.responseHeaders(map),
+                     baseUrl + "/response-headers");
+        map.put("k1", "v1");
+        assertEquals(HttpbinRequestBuilder.responseHeaders(map),
+                     baseUrl + "/response-headers?k1=v1");
+        map.put("k2", "v2");
+        assertEquals(HttpbinRequestBuilder.responseHeaders(map),
+                     baseUrl + "/response-headers?k1=v1&k2=v2");
+
+        // redirect
+        assertEquals(HttpbinRequestBuilder.redirect(3), 
+                     baseUrl + "/redirect/3");
+
+        // redirect-to
+        assertEquals(HttpbinRequestBuilder.redirectTo("foo"),
+                     baseUrl + "/redirect-to?url=foo");
+
+        // relative-redirect
+        assertEquals(HttpbinRequestBuilder.relativeRedirect(4),
+                     baseUrl + "/relative-redirect/4");
+
+        // cookies/set
+        assertEquals(HttpbinRequestBuilder.cookiesSet(map),
+                     baseUrl + "/cookies/set?k1=v1&k2=v2");
+
+        // cookies/delete
+        assertEquals(HttpbinRequestBuilder.cookiesDelete(), 
+                     baseUrl + "/cookies/delete");
+        assertEquals(HttpbinRequestBuilder.cookiesDelete("c1"), 
+                     baseUrl + "/cookies/delete?c1");
+        assertEquals(HttpbinRequestBuilder.cookiesDelete("c1", "c2"), 
+                     baseUrl + "/cookies/delete?c1&c2");
+
+        // basic-auth
+        assertEquals(HttpbinRequestBuilder.basicAuth("u", "p"), 
+                     baseUrl + "/basic-auth/u/p");
+        
+        // hidden-basic-auth
+        assertEquals(HttpbinRequestBuilder.hiddenBasicAuth("u", "p"), 
+                     baseUrl + "/hidden-basic-auth/u/p");
+
+        // digest-auth
+        assertEquals(HttpbinRequestBuilder.digestAuth("q", "u", "p"), 
+                     baseUrl + "/digest-auth/q/u/p");
+
+        // stream
+        assertEquals(HttpbinRequestBuilder.stream(5),
+                     baseUrl + "/stream/5");
+
+        // delay
+        assertEquals(HttpbinRequestBuilder.delay(6),
+                     baseUrl + "/delay/6");
+
+        // drip
+        assertEquals(HttpbinRequestBuilder.drip(7, 8, 9),
+                     baseUrl + "/drip?numbytes=7&duration=8&delay=9");
+    }
+
+    private void assertEquals(Object a, Object b) {
+        Assert.assertEquals(a, b);
+    }
+
 } // HttpbinRequestBuilderTest
